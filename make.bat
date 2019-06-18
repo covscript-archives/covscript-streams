3ca698237e3c0c1afd48f2c3a3fc8233d088b670
imkiva <libkernelpanic@gmail.com>
2019-06-18 23:57:14 +0800

    cmake: update cmake for auto build

--- make.bat
+++
@@ -1,4 +0,0 @@
-@mkdir build
-@cd build
-@mkdir imports
-@g++ -std=c++14 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\streams.cpp -o .\imports\streams.cse -L%CS_DEV_PATH%\lib -lcovscript
